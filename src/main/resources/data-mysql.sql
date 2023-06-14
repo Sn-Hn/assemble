8b5001a491f27f65c1bd09e9e8624643556663d4
ShinHan <tlsgks8668@naver.com>
2023-06-15 00:27:01 +0900

    fix: default 데이터 세팅 수정
    - Post 삭제

--- src/main/resources/data-mysql.sql
+++ src/main/resources/data-mysql.sql
@@ -2,7 +2,3 @@ INSERT INTO users(user_name, email, password, phone_number) VALUES ('tester00','
 INSERT INTO users(user_name, email, password, phone_number) VALUES ('tester01','test01@gmail.com','$2a$10$StGFrD9mIqLFRBaBsvVDuusb54PQMonzjtc/3DbNKJNXAcvY7M.Ey', '01000000000'); /* password2! */
 INSERT INTO users(user_name, email, password, phone_number) VALUES ('tester02','test02@gmail.com','$2a$10$8ALIty0h4ebK4HIPVpWDRu7/1cP8CIs9odtGiybmBajNv3qenfid6', '01000000000'); /* password3! */
 
-INSERT INTO post(post_name, post_content, user_email) VALUES ('1번 게시글', '1번 게시글 내용 !!!', 'test00@gmail.com');
-INSERT INTO post(post_name, post_content, user_email) VALUES ('2번 게시글', '2번 게시글 내용 !!!', 'test01@gmail.com');
-INSERT INTO post(post_name, post_content, user_email) VALUES ('3번 게시글', '3번 게시글 내용 !!!', 'test02@gmail.com');
-
