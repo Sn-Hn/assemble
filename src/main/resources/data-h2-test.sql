2a4b2835698d3f1254599093e517a654fcf86678
ShinHan <tlsgks8668@naver.com>
2023-06-14 23:54:28 +0900

    build: application 설정
    - gitignore 적용
    - 의존성 주입
      - spring-boot-starter-web
      - springfox-boot-starter (swagger)
      - mockmvc
      - lombok
    - default 데이터 추가

---
+++ src/main/resources/data-h2-test.sql
@@ -0,0 +1,8 @@
+INSERT INTO users(user_name,email,password) VALUES ('tester00','test00@gmail.com','$2a$10$c0kMDetZjkkKIBdEeSH4oO60xxkt9A9FObc7CApbyZJ5JEt/ie3L2'); /* password1! */
+INSERT INTO users(user_name,email,password) VALUES ('tester01','test01@gmail.com','$2a$10$StGFrD9mIqLFRBaBsvVDuusb54PQMonzjtc/3DbNKJNXAcvY7M.Ey'); /* password2! */
+INSERT INTO users(user_name,email,password) VALUES ('tester02','test02@gmail.com','$2a$10$8ALIty0h4ebK4HIPVpWDRu7/1cP8CIs9odtGiybmBajNv3qenfid6'); /* password3! */
+
+INSERT INTO post(post_name, post_content, writer_email) VALUES ('1번 게시글', '1번 게시글 내용 !!!', 'test00@gmail.com');
+INSERT INTO post(post_name, post_content, writer_email) VALUES ('2번 게시글', '2번 게시글 내용 !!!', 'test01@gmail.com');
+INSERT INTO post(post_name, post_content, writer_email) VALUES ('3번 게시글', '3번 게시글 내용 !!!', 'test02@gmail.com');
+
---
+++ src/main/resources/data-mysql.sql
@@ -0,0 +1,8 @@
+INSERT INTO users(user_name, user_email, password, phone_number) VALUES ('tester00','test00@gmail.com','$2a$10$c0kMDetZjkkKIBdEeSH4oO60xxkt9A9FObc7CApbyZJ5JEt/ie3L2', '01000000000'); /* password1! */
+INSERT INTO users(user_name, user_email, password, phone_number) VALUES ('tester01','test01@gmail.com','$2a$10$StGFrD9mIqLFRBaBsvVDuusb54PQMonzjtc/3DbNKJNXAcvY7M.Ey', '01000000000'); /* password2! */
+INSERT INTO users(user_name, user_email, password, phone_number) VALUES ('tester02','test02@gmail.com','$2a$10$8ALIty0h4ebK4HIPVpWDRu7/1cP8CIs9odtGiybmBajNv3qenfid6', '01000000000'); /* password3! */
+
+INSERT INTO post(post_name, post_content, user_email) VALUES ('1번 게시글', '1번 게시글 내용 !!!', 'test00@gmail.com');
+INSERT INTO post(post_name, post_content, user_email) VALUES ('2번 게시글', '2번 게시글 내용 !!!', 'test01@gmail.com');
+INSERT INTO post(post_name, post_content, user_email) VALUES ('3번 게시글', '3번 게시글 내용 !!!', 'test02@gmail.com');
+
