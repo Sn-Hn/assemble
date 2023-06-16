2e0d7fbef1a907c7ea5a63863eca52eb925ccca7
ShinHan <tlsgks8668@naver.com>
2023-06-17 01:01:19 +0900

    chore: default 데이터 수정

--- src/main/resources/data-mysql.sql
+++ src/main/resources/data-mysql.sql
@@ -1,4 +1,31 @@
-INSERT INTO users(user_name, email, password, phone_number) VALUES ('tester00','test00@gmail.com','$2a$10$c0kMDetZjkkKIBdEeSH4oO60xxkt9A9FObc7CApbyZJ5JEt/ie3L2', '01000000000'); /* password1! */
-INSERT INTO users(user_name, email, password, phone_number) VALUES ('tester01','test01@gmail.com','$2a$10$StGFrD9mIqLFRBaBsvVDuusb54PQMonzjtc/3DbNKJNXAcvY7M.Ey', '01000000000'); /* password2! */
-INSERT INTO users(user_name, email, password, phone_number) VALUES ('tester02','test02@gmail.com','$2a$10$8ALIty0h4ebK4HIPVpWDRu7/1cP8CIs9odtGiybmBajNv3qenfid6', '01000000000'); /* password3! */
+INSERT INTO users(user_name, email, password, phone_number, role, nickname, created_date, modified_date)
+VALUES (
+        'tester00',
+        'test00@gmail.com',
+        '$2a$10$c0kMDetZjkkKIBdEeSH4oO60xxkt9A9FObc7CApbyZJ5JEt/ie3L2',
+        '01000000000',
+        'VIP',
+        'test01',
+        now(),
+        now()); /* password1! */
+
+INSERT INTO users(user_name, email, password, phone_number, role, nickname, created_date, modified_date)
+VALUES ('tester01',
+        'test01@gmail.com',
+        '$2a$10$StGFrD9mIqLFRBaBsvVDuusb54PQMonzjtc/3DbNKJNXAcvY7M.Ey',
+        '01000000000',
+        'USER',
+        'test02',
+        now(),
+        now()); /* password2! */
+
+INSERT INTO users(user_name, email, password, phone_number, role, nickname, created_date, modified_date)
+VALUES ('tester02',
+        'test02@gmail.com',
+        '$2a$10$8ALIty0h4ebK4HIPVpWDRu7/1cP8CIs9odtGiybmBajNv3qenfid6',
+        '01000000000',
+        'USER',
+        'test03',
+        now(),
+        now()); /* password3! */
 
