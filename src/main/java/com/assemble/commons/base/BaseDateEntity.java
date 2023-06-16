aa112731d3ebb9a4cc6b7efa62f173af2f5493e7
ShinHan <tlsgks8668@naver.com>
2023-06-17 01:10:03 +0900

    feat: BaseEntity 구현
    - 작성일, 수정일 (BaseDateEntity)
    - 작성자, 수정자 (BaseUserEntity - BaseDateEntity 상속)

---
+++ src/main/java/com/assemble/commons/base/BaseDateEntity.java
@@ -0,0 +1,36 @@
+package com.assemble.commons.base;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseDateEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public LocalDateTime getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public void setModifiedDate(LocalDateTime modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+}
---
+++ src/main/java/com/assemble/commons/base/BaseUserEntity.java
@@ -0,0 +1,19 @@
+package com.assemble.commons.base;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseUserEntity extends BaseDateEntity {
+
+    @CreatedBy
+    private Long creator;
+
+    @LastModifiedBy
+    private Long modifier;
+}
