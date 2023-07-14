4fa63c8ff77826368fd1c7f04baa00bc44c441d2
ShinHan <tlsgks8668@naver.com>
2023-07-14 22:47:17 +0900

    chore: gradle-wrapper 추가
    refactor: TODO 제거

--- src/main/java/com/assemble/util/JwtUtils.java
+++ src/main/java/com/assemble/util/JwtUtils.java
@@ -17,7 +17,6 @@ public class JwtUtils {
         return getTokenAccount(request, JwtType.REFRESH_TOKEN);
     }
 
-    // TODO: 2023-07-09 JWT를 Header에 넣을지, Cookie에 넣을지 토의 -신한
     public static String getTokenAccount(HttpServletRequest request, JwtType jwtType) {
         return Arrays.stream(request.getCookies())
                 .filter(cookies -> cookies.getName().equals(jwtType.getCode()))
