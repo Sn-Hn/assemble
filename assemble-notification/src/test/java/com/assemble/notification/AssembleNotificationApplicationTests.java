708d388e5fd2e4286a34feb8b01573da32f6f8da
ShinHan <tlsgks8668@naver.com>
2023-10-05 17:14:38 +0900

    feat: 알림 모듈 생성
    - assemble-app
      - assemble-api
      - assemble-notification

---
+++ assemble-notification/src/test/java/com/assemble/notification/AssembleNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assemble.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssembleNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
