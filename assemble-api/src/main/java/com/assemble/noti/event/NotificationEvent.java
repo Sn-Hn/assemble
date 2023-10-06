923052291e3a8c9778de572fdd5a5eb2a3202f88
ShinHan <tlsgks8668@naver.com>
2023-10-07 02:48:34 +0900

    feat: SQS Event 추가

---
+++ assemble-api/src/main/java/com/assemble/noti/event/NotificationEvent.java
@@ -0,0 +1,35 @@
+package com.assemble.noti.event;
+
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.assemble.noti.dto.WebNotificationRequest;
+import com.assemble.util.MessageUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificationEvent {
+
+    @Value("${cloud.aws.sqs.queue-name}")
+    private String QUEUE_NAME;
+
+    private final QueueMessagingTemplate queueMessagingTemplate;
+
+    public NotificationEvent(AmazonSQS amazonSQS) {
+        this.queueMessagingTemplate = new QueueMessagingTemplate((AmazonSQSAsync) amazonSQS);
+    }
+
+    public void publish(String message, Object... args) {
+        Message<String> sendMessage = MessageBuilder.withPayload(MessageUtils.getMessage(message, args)).build();
+        queueMessagingTemplate.convertAndSend(QUEUE_NAME, sendMessage);
+    }
+
+    public void publish(Long userId, String message, String fcmToken) {
+        WebNotificationRequest webNotificationRequest =
+                new WebNotificationRequest(String.valueOf(userId), message, fcmToken);
+        queueMessagingTemplate.convertAndSend(QUEUE_NAME, webNotificationRequest);
+    }
+}
