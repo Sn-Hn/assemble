76e4e5f1f2d65a7cf88f82e67dcda8d1f2d78ff8
ShinHan <tlsgks8668@naver.com>
2023-11-19 02:49:17 +0900

    feat: Spring actuator 추가

--- assemble-api/src/main/java/com/assemble/commons/config/SwaggerConfig.java
+++ assemble-api/src/main/java/com/assemble/commons/config/SwaggerConfig.java
@@ -2,9 +2,13 @@ package com.assemble.commons.config;
 
 import com.assemble.commons.converter.PageableConverter;
 import com.fasterxml.classmate.TypeResolver;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Pageable;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -16,9 +20,13 @@ import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spring.web.plugins.WebFluxRequestHandlerProvider;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @EnableSwagger2
 @Configuration
@@ -67,5 +75,38 @@ public class SwaggerConfig {
                 .license("Assemble")
                 .version("1.0")
                 .build();
+    }
+
+    @Bean
+    public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+        return new BeanPostProcessor() {
+
+            @Override
+            public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+                if (bean instanceof WebMvcRequestHandlerProvider || bean instanceof WebFluxRequestHandlerProvider) {
+                    customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
+                }
+                return bean;
+            }
+
+            private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
+                List<T> copy = mappings.stream()
+                        .filter(mapping -> mapping.getPatternParser() == null)
+                        .collect(Collectors.toList());
+                mappings.clear();
+                mappings.addAll(copy);
+            }
+
+            @SuppressWarnings("unchecked")
+            private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
+                try {
+                    Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+                    field.setAccessible(true);
+                    return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
+                } catch (IllegalArgumentException | IllegalAccessException e) {
+                    throw new IllegalStateException(e);
+                }
+            }
+        };
     }
 }
--- assemble-api/src/main/java/com/assemble/commons/filter/JwtFilter.java
+++ assemble-api/src/main/java/com/assemble/commons/filter/JwtFilter.java
@@ -1,8 +1,12 @@
 package com.assemble.commons.filter;
 
 import com.assemble.auth.domain.JwtProvider;
+import com.assemble.commons.exception.NotAdminException;
+import com.assemble.commons.exception.NotFoundException;
 import com.assemble.commons.exception.UnauthorizedException;
 import com.assemble.commons.exclusion.ExclusionApis;
+import com.assemble.user.entity.User;
+import com.assemble.user.repository.UserRepository;
 import com.assemble.util.JwtUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -28,12 +32,16 @@ public class JwtFilter extends OncePerRequestFilter {
 
     private final ExclusionApis exclusionApis;
 
+    private final UserRepository userRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String accessToken = JwtUtils.getAccessTokenFromHeader(request);
         if (!jwtProvider.isValidToken(accessToken)) {
             throw new UnauthorizedException("Unauthorization", "Invalid Access Token !");
         }
+
+        accessActuator(request, accessToken);
 
         log.info("Request UserId={}, Email={}", jwtProvider.getSubject(accessToken), jwtProvider.getEmail(accessToken));
 
@@ -55,5 +63,22 @@ public class JwtFilter extends OncePerRequestFilter {
         return exclusionApi.keySet().stream()
                 .anyMatch(key -> matcher.match(key, servletPath)
                         && exclusionApi.get(key).contains(method.toUpperCase()));
+    }
+
+    private void accessActuator(HttpServletRequest request, String accessToken) {
+        String servletPath = request.getServletPath();
+        if (servletPath.contains("actuator")) {
+            checkAdmin(accessToken);
+        }
+    }
+
+    private void checkAdmin(String accessToken) {
+        Long userId = Long.valueOf(jwtProvider.getSubject(accessToken));
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException(User.class, userId));
+
+        if (!user.isAdmin()) {
+            throw new NotAdminException();
+        }
     }
 }
