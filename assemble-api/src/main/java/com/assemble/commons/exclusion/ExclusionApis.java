6259692beaf0001578cd277b2011547330298092
ShinHan <tlsgks8668@naver.com>
2023-11-19 16:19:31 +0900

    fix: JWT 검증 제외 API 이동

--- assemble-api/src/main/java/com/assemble/commons/exclusion/ExclusionApis.java
+++ assemble-api/src/main/java/com/assemble/commons/exclusion/ExclusionApis.java
@@ -1,31 +1,17 @@
 package com.assemble.commons.exclusion;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class ExclusionApis {
+
+    @Value("#{${jwt.exclusions}}")
     private Map<String, String> exclusionApis;
 
     private ExclusionApis() {
-        Map<String, String> apis = new HashMap<>();
-        apis.put("/images/**", "GET");
-        apis.put("/authentication", "POST");
-        apis.put("/signup", "POST");
-        apis.put("/email/validation", "GET");
-        apis.put("/nickname/validation", "GET");
-        apis.put("/meeting", "GET");
-        apis.put("/meeting/*", "GET");
-        apis.put("/category", "GET");
-        apis.put("/auth/token", "POST");
-        apis.put("/user/validation", "POST");
-        apis.put("/user/email", "GET");
-        apis.put("/user/password", "PUT");
-        apis.put("/notification", "GET");
-
-        this.exclusionApis = apis;
     }
 
     public Map<String, String> getExclusionApis() {
